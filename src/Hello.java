aefa461fa9b1df8d1e69ec30045069ee522dd9d3
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-03-27 10:38:27 +0900

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요!");
+
+        int i = 100;
+
+        String 김철수 = "김철수";
+
+        double rn = Math.random();
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
+
+
+    
