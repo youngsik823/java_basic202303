f4a9b45b4b817b04e8f5041a1f1739532a2a75e1
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-03 17:51:25 +0900

    java

--- src/day03/ArraySearch.java
+++ src/day03/ArraySearch.java
@@ -1,30 +1,29 @@
-//package day03;
-//
-//import java.util.Arrays;
-//
-//public class ArraySearch {
-//    String[] foods = {"삼겹살", "족발", "피자", "햄버거"};
-//
-//    public static int indexOf(String target) {
-//        // 인덱스 탐색 알고리즘
+package day03;
+
+public class ArraySearch {
+    static String[] foods = {"삼겹살", "족발", "피자", "햄버거"};
+
+    public static int indexOf(String target) {
+        // 인덱스 탐색 알고리즘
 //        int index = -1;
-//        for (int i = 0; i < foods.length; i++) {
-//            if (target.equals(foods[i])) {
+        for (int i = 0; i < foods.length; i++) {
+            if (target.equals(foods[i])) {
 //                index = i;
 //                break;
-//            }
-//
-//        }
-//        return index;
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        foods[0] = foods[1];
-//        foods[2] = "치킨";
-//        int index = indexOf("족발");
-//
-//
-//        System.out.println("찾은 인덱스: " + index);
-//    }
-//}
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+
+        foods[2] = "치킨";
+
+        int index = indexOf("족발");
+
+        System.out.println("찾은 인덱스: " + index);
+
+    }
+}
